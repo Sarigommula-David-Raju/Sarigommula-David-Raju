35c66a3788b243800f9f42ea13849ae132ec2dfc
Sarigommula-David-Raju <166585452+Sarigommula-David-Raju@users.noreply.github.com>
2025-04-17 09:57:52 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,50 @@
+let inputBox= document.getElementById("input-box");
+let listContainer =  document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Write something!");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+}, false);
+
+// listContainer.addEventListener("dblclick", function(e){
+//     if(e.target.tagName === "LI"){
+//         e.target.classList.remove("checked");
+//         saveData();
+//     } else if(e.target.tagName === "SPAN"){
+//         e.target.parentElement.remove();
+//         saveData();
+//     }
+    
+// }, false);
+
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showtask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
